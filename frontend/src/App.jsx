dd8d40aaceb238a28ec759d2ccb8fb10f9de98b2
matiasrivero-ctrl <matiasariveros.cinco@gmail.com>
2022-05-08 23:50:14 -0300

    connecting eth object to front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const checkIfWalletIsConnected = () => {
+    const {ethereum} = window;
+    
+    if(!ethereum) {
+      console.log("Make sure you have metamask!");
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className='App'>
       <h1>Welcome to web3</h1>
